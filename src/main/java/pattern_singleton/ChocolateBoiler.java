3d0765a9e871988c31090bf4a3eca5d533f09f3f
connieya <gunny6026@naver.com>
2023-08-16 11:16:22 +0900

    헤드퍼스트 디자인패턴 15일차 (p.205 ~ 216)

---
+++ src/main/java/pattern_singleton/ChocolateBoiler.java
@@ -0,0 +1,48 @@
+package pattern_singleton;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+
+    private static ChocolateBoiler chocolateBoiler;
+    public static ChocolateBoiler getInstance() {
+        if(chocolateBoiler == null) {
+            chocolateBoiler = new ChocolateBoiler();
+        }
+        return chocolateBoiler;
+    }
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public void fill() {
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+            // 보일러에 우유와 초콜릿을 혼합한 재료를 넣음
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            // 끓인 재료를 다음 단계로 넘김
+            empty = true;
+        }
+    }
+    public void boil() {
+        if(!isEmpty() && !isBoiled()) {
+            // 재료를 끓임
+            boiled = true;
+        }
+    }
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
