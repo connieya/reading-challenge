b81280913926a9fc46791487e0a0b09d5a6b8986
connieya <gunny6026@naver.com>
2023-08-17 11:26:04 +0900

    헤드퍼스트 디자인패턴 16일차 (p.217 ~ 226)

--- src/main/java/pattern_singleton/Singleton.java
+++ src/main/java/pattern_singleton/Singleton.java
@@ -2,18 +2,19 @@ package pattern_singleton;
 
 public class Singleton {
     // Singleton 클래스의 하나뿐인 인스턴스를 저장하는 정적 변수
-    private static Singleton uniqueInstance;
-
-    // 기타 인스턴스 변수
+    private static  Singleton instance;
 
 
-    // 생성자를 private으로 선언했으므로 Singleton 에서만 클래스의 인스턴스를 만들 수 있다.
     private Singleton() {}
 
-    public static Singleton getInstance() {
-        if(uniqueInstance == null) {
-            uniqueInstance = new Singleton();
+    public static  Singleton getInstance() {
+        if(instance == null){
+            synchronized (Singleton.class) {
+                if(instance == null) {
+                    instance = new Singleton();
+                }
+            }
         }
-        return uniqueInstance;
+        return instance;
     }
 }
