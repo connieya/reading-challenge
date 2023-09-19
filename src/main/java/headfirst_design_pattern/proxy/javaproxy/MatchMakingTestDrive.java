acd8c2f356d3c9a9eaa719b0c4c1bdb0b4c3a03c
connieya <gunny6026@naver.com>
2023-09-19 11:06:15 +0900

    헤드퍼스트 디자인 패턴 38일차 (p.503 ~ 513)

---
+++ src/main/java/headfirst_design_pattern/proxy/javaproxy/MatchMakingTestDrive.java
@@ -0,0 +1,12 @@
+package headfirst_design_pattern.proxy.javaproxy;
+
+public class MatchMakingTestDrive {
+    public static void main(String[] args) {
+        MatchMakingTestDrive test = new MatchMakingTestDrive();
+        test.drive();
+    }
+
+    private void drive() {
+
+    }
+}
