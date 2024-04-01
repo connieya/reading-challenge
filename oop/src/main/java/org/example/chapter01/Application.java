d5152ff627935f448f024f155c5983e7489aadf9
connieya <gunny6026@naver.com>
2024-04-01 20:48:47 +0900

    단위 테스트 패턴 1일차 (p.29 ~ 50)

--- oop/src/main/java/org/example/chapter01/Application.java
+++ oop/src/main/java/org/example/chapter01/Application.java
@@ -10,25 +10,25 @@ public class Application  {
     private ScreenUI currentScreen = null;
 
     public Application() {
-        menu1.setOnClickListener(this);
-        menu2.setOnClickListener(this);
-        button1.setOnClickListener(this);
+//        menu1.setOnClickListener(this);
+//        menu2.setOnClickListener(this);
+//        button1.setOnClickListener(this);
     }
 
-    public void clicked(Component eventSource){
-        String sourceId = eventSource.getId();
-        if (sourceId.equals("menu1")){
-            currentScreen = new Menu1ScreenUI();
-            currentScreen.show();
-        }else if(sourceId.equals("menu2")){
-           currentScreen = new Menu2ScreenUI();
-           currentScreen.show();
-        }else if(sourceId.equals("button1")){
-            if(currentScreen == null){
-                return;;
-            }
-            currentScreen.handleButton1Click();
-
-        }
-    }
+//    public void clicked(Component eventSource){
+//        String sourceId = eventSource.getId();
+//        if (sourceId.equals("menu1")){
+//            currentScreen = new Menu1ScreenUI();
+//            currentScreen.show();
+//        }else if(sourceId.equals("menu2")){
+//           currentScreen = new Menu2ScreenUI();
+//           currentScreen.show();
+//        }else if(sourceId.equals("button1")){
+//            if(currentScreen == null){
+//                return;;
+//            }
+//            currentScreen.handleButton1Click();
+//
+//        }
+//    }
 }
