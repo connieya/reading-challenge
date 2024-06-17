19a74a9b260df2bb1018625c38f80a57088f9d44
cony <geonhee.park@snowoncard.com>
2024-06-17 23:12:22 +0900

    EFFECTIVE JAVA 아이템 47. 반환 타입으로는 스트림보다 컬렉션이 낫다.

---
+++ effective-java/src/main/java/org/example/item47/SubLists.java
@@ -0,0 +1,24 @@
+package src.main.java.org.example.item47;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class SubLists {
+    public static <E> Stream<List<E>> of(List<E> list) {
+        return Stream.concat(Stream.of(Collections.emptyList()) ,
+                prefixes(list).flatMap(SubLists::suffixes));
+    }
+
+
+    private static <E> Stream<List<E>> suffixes(List<E> list) {
+        return IntStream.range(0 , list.size())
+                .mapToObj(start -> list.subList(start, list.size()));
+    }
+
+    private static <E> Stream<List<E>> prefixes(List<E> list) {
+        return IntStream.rangeClosed(1,list.size())
+                .mapToObj(end -> list.subList(0,end));
+    }
+}
