82f3245cfaab9cf6510f8e6be63e8d1a78f9b845
cony <geonhee.park@snowoncard.com>
2024-07-22 11:09:14 +0900

    EFFECTIVE JAVA - 아이템 89. 인스턴스 수를 통제해야 한다면 readResolve 보다는 열거 타입을 사용하라

---
+++ effective-java/src/main/java/org/example/item89/Elvis.java
@@ -0,0 +1,21 @@
+package org.example.item89;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class Elvis implements Serializable {
+    public static final Elvis INSTANCE = new Elvis();
+
+    private Elvis() {}
+
+    private String[] favoriteSongs =
+            {"Hound Dog" , "Heartbreak Hotel"};
+
+    public void printFavorites() {
+        System.out.println(Arrays.toString(favoriteSongs));
+    }
+
+    private Object readResolve(){
+        return INSTANCE;
+    }
+}
---
+++ effective-java/src/main/java/org/example/item89/ElvisImpersonator.java
@@ -0,0 +1,8 @@
+package org.example.item89;
+
+public class ElvisImpersonator {
+
+    private static final byte[] serializedForm = {
+            (byte) 0xac, (byte) 0xed, 0x00, 0x05
+    };
+}
