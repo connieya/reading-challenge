a24c646456f11e3136fe6f9f8f192c8c422a0c75
cony <geonhee.park@snowoncard.com>
2024-07-22 10:21:40 +0900

    EFFECTIVE JAVA -  아이템 88. readObject 메서드는 방어적으로 작성하라

---
+++ effective-java/src/main/java/org/example/item88/Period.java
@@ -0,0 +1,29 @@
+package org.example.item88;
+
+import java.util.Date;
+
+public final class Period {
+    private final Date start;
+    private final Date end;
+
+    public Period(Date start, Date end) {
+        this.start = new Date(start.getTime());
+        this.end = new Date(end.getTime());
+
+        if (start.compareTo(end) > 0)
+            throw new IllegalArgumentException(start + "가 " + end + " 보다 늦다.");
+    }
+
+    public Date start() {
+        return new Date(start.getTime());
+    }
+
+    public Date end() {
+        return new Date(end.getTime());
+    }
+
+    @Override
+    public String toString() {
+        return start + " - " + end;
+    }
+}
